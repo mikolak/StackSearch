1673ac3dd49eab5cde1e4a2c60f7ee930a791fbd
Mikołaj Kojdecki <mikolak.k@gmail.com>
2016-02-15 19:29:32 +0100

    Init and UI sketch

---
+++ app/src/main/java/com/kojdecki/stacksearch/MainActivity.java
@@ -0,0 +1,72 @@
+package com.kojdecki.stacksearch;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
+
+
+public class MainActivity extends Activity implements DetailsFragment.OnFragmentInteractionListener {
+
+    private static final String TAG = "MainActivity:";
+    private String mLastQuery = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SearchView searchView = (SearchView) findViewById(R.id.search_box);
+        searchView.setSubmitButtonEnabled(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //TODO
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //TODO implement this
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/com/kojdecki/stacksearch/views/FloatingButton.java
@@ -0,0 +1,16 @@
+package com.kojdecki.stacksearch.views;
+
+import android.content.Context;
+import android.widget.Button;
+
+/**
+ * Created by calot on 2/15/16.
+ */
+public class FloatingButton extends Button {
+
+    public FloatingButton(Context context) {
+        super(context);
+    }
+
+
+}
