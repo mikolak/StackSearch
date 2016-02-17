d2da6ab271d383576a75ce3560b058738e67a937
Mikołaj Kojdecki <mikolak.k@gmail.com>
2016-02-17 20:49:19 +0100

    Added fetching additional pages of the results

--- app/src/main/java/com/kojdecki/stacksearch/MainActivity.java
+++ app/src/main/java/com/kojdecki/stacksearch/MainActivity.java
@@ -2,14 +2,17 @@ package com.kojdecki.stacksearch;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.kojdecki.stacksearch.retrofit.Question;
 import com.kojdecki.stacksearch.retrofit.Search;
@@ -28,7 +31,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends Activity implements Callback<Search>, DetailsFragment.OnFragmentInteractionListener {
 
-    private static final String TAG = "MainActivity:";
+    //private static final String TAG = "MainActivity:";
     private String mLastQuery = null;
     private Call<Search> mCall = null;
     private boolean mCallRunning = false;
@@ -45,6 +48,22 @@ public class MainActivity extends Activity implements Callback<Search>, DetailsF
         final ListView listView = (ListView) findViewById(R.id.results_list);
         mQuestionListAdapter = new QuestionListAdapter(new ArrayList<Question>(), this);
         listView.setAdapter(mQuestionListAdapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                //not needed
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount == totalItemCount
+                        && totalItemCount > 1
+                        && mPageNumber > 1
+                        && !mCallRunning) {
+                    searchStackOverflow(mPageNumber);
+                }
+            }
+        });
 
         final SearchView searchView = (SearchView) findViewById(R.id.search_box);
         searchView.setSubmitButtonEnabled(false);
@@ -53,15 +72,15 @@ public class MainActivity extends Activity implements Callback<Search>, DetailsF
             public boolean onQueryTextSubmit(String query) {
                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
-//                //Query doesn't work if it ends with a space
-//                if (query.endsWith(" "))
-//                    query = query.substring(0, query.length() - 1);
                 mLastQuery = query;
                 mPageNumber = 1;
                 mQuestionListAdapter.clear();
-                footerView = new ProgressBar(MainActivity.this);
-                listView.addFooterView(footerView);
-                searchStackOverflow(mPageNumber);
+
+                if (isNetworkConnected())
+                    searchStackOverflow(mPageNumber);
+                else {
+                    Toast.makeText(MainActivity.this, "No network connection", Toast.LENGTH_SHORT).show();
+                }
                 return true;
             }
 
@@ -72,17 +91,29 @@ public class MainActivity extends Activity implements Callback<Search>, DetailsF
         });
     }
 
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) MainActivity.this.
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        return activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+    }
+
     private void searchStackOverflow(int page) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.stackexchange.com")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        ListView listView = (ListView) findViewById(R.id.results_list);
+        footerView = new ProgressBar(MainActivity.this);
+        listView.addFooterView(footerView);
+
         StackOverflowAPI stackOverflowAPI = retrofit.create(StackOverflowAPI.class);
         mCall = stackOverflowAPI.loadSearch(mLastQuery.replace(" ", ","), page);
         mCall.enqueue(this);
         mCallRunning = true;
-        return;
     }
 
     @Override
@@ -94,17 +125,20 @@ public class MainActivity extends Activity implements Callback<Search>, DetailsF
     public void onResponse(Call<Search> call, Response<Search> response) {
         ListView listView = (ListView) findViewById(R.id.results_list);
         mQuestionListAdapter.addAll(response.body().getItems());
-        if (mQuestionListAdapter.getCount() == 0)
+        if (mQuestionListAdapter.getCount() == 0) {
             mQuestionListAdapter.add(new Question(-1, new ShallowUser(null, null), "No hits for this search!"));
+            mPageNumber = 1;
+        } else {
+            mPageNumber += 1;
+        }
         mCallRunning = false;
-        Log.d(TAG + ":onRespon", response.body().toString());
         listView.removeFooterView(footerView);
     }
 
     @Override
     public void onFailure(Call<Search> call, Throwable t) {
         mCallRunning = false;
-        //TODO snackbar with retry
+        Toast.makeText(this, "Application failed to fetch results", Toast.LENGTH_SHORT).show();
         ListView listView = (ListView) findViewById(R.id.results_list);
         listView.removeFooterView(footerView);
     }
@@ -112,10 +146,8 @@ public class MainActivity extends Activity implements Callback<Search>, DetailsF
     @Override
     protected void onResume() {
         super.onResume();
-        //TODO check internet connection and display a dialog if need be (ONCE, we don't wanna pest the user)
         if(mLastQuery != null && mCallRunning) {
             searchStackOverflow(mPageNumber);
-            //TODO set refeshing icon in the ListView
         }
     }
 
@@ -143,30 +175,7 @@ public class MainActivity extends Activity implements Callback<Search>, DetailsF
         mCallRunning = savedInstanceState.getBoolean("mCallRunning", false);
         mLastQuery = savedInstanceState.getString("mLastQuery", null);
         mPageNumber = savedInstanceState.getShort("mPageNumber", (short) 1);
-        ListView listView = (ListView) findViewById(R.id.results_list);
         mQuestionListAdapter
                 .addAll((ArrayList<Question>) savedInstanceState.getSerializable("AdapterItems"));
     }
-
-    //    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
--- app/src/main/java/com/kojdecki/stacksearch/views/FloatingButton.java
+++ app/src/main/java/com/kojdecki/stacksearch/views/FloatingButton.java
@@ -13,4 +13,6 @@ public class FloatingButton extends Button {
     }
 
 
+
+
 }
